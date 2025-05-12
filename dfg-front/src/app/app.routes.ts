cd4a1662a71abbcadb3ad3d2f394231627e1bdbd
mkrysiak <mkrysiak@consdata.com>
2025-05-12 17:06:00 +0200

    [ICL-7973] Possibility to specify error type in path, small tweaks

--- dfg-front/src/app/app.routes.ts
+++ dfg-front/src/app/app.routes.ts
@@ -1,9 +1,9 @@
-import { Routes } from '@angular/router';
-import { ErrorComponent } from './error/error.component';
+import {Routes} from '@angular/router';
+import {ErrorComponent} from './error/error.component';
 import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'error', component: ErrorComponent },
-  { path: '**', redirectTo: '/error' }
+  { path: 'error/:type', component: ErrorComponent },
+  { path: '**', redirectTo: '' }
 ];
