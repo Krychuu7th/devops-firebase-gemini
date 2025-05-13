b7827a9c8748679c9b532bf687379c6e9864dd22
bdomzalski <bdomzalski@consdata.com>
2025-05-13 11:04:17 +0200

    ICL-7945 add user name

--- dfg-front/src/app/app.config.ts
+++ dfg-front/src/app/app.config.ts
@@ -6,11 +6,13 @@ import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {environment} from '../environment/environment';
+import {getDatabase, provideDatabase} from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth())]
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase())]
 };
