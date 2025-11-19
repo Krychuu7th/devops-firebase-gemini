29362568e9bc0f43a73bf35df35de89974e0b7ee
mkrysiak <mkrysiak@consdata.com>
2025-11-19 16:18:38 +0100

    [ICL-8145] Terraform Firebase AI Logic resources

--- terraform/gemini.tf
+++
@@ -1,5 +0,0 @@
-# Terraform ma póki co ograniczone wsparcie dla Firebase AI Logic.
-# Należy monitorwać dokumentację dla providera google-beta
-# pod kątem pojawiania się tam zasobów związanych z Firebase AI Logic.
-
-# Konfiguracja odbyłą się ręcznie, można ją pewnie zautomatyzować za pomocą null_resource i gcloud CLI
